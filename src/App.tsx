8ac806b991a37f93d804a7501902b52f38acff89
Alex <t9371100211@gmail.com>
2021-10-11 15:29:27 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {v1} from "uuid";
+
+
+type TaskType = {id: string, title: string, isDone: boolean}
+
+type TasksStateType = {
+  [todoListId: string]: TaskType[]
+}
+
+type FilterValueType = 'all' | 'completed' | 'active'
+type TodoListType = {id: string, title: string, filter: FilterValueType}
 
 function App() {
+  const [todoListId1, todoListId2] = [v1(), v1()]
+  let [todoLists, setTodoLists] = useState<Array<TodoListType>>([
+    {id: todoListId1, title: "What to learn", filter: "all"},
+    {id: todoListId2, title: "What to buy", filter: "all"}
+  ])
+
+  let [tasks, setTasks] = useState<TasksStateType>({
+    [todoListId1]: [
+      {id: v1(), title: "HTML&CSS", isDone: true},
+      {id: v1(), title: "JS", isDone: true}
+    ],
+    [todoListId2]: [
+      {id: v1(), title: "Milk", isDone: true},
+      {id: v1(), title: "React Book", isDone: true}
+    ]
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
