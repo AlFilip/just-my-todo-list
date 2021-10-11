48ed54accaad1ec7f6f774e48befc8edd223a51a
Alex <t9371100211@gmail.com>
2021-10-11 15:43:23 +0300

    Second commit

--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import s from './TodoList.module.css'
+
+
 
 export const TodoList = () => {
     return (
-        <div>
-
+        <div className={s.todoList}>
+            <div className={s.header}>
+                Header
+            </div>
+            <div className={s.body}>
+                body
+            </div>
+            <div className={s.filters}>
+                buttons
+            </div>
         </div>
     )
 }
